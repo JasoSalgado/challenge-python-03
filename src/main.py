05f3c74b4ff6f924c9f2a211705e8c8f965d1cc0
Jaso Salgado <jaso_98@hotmail.com>
2020-05-17 12:46:08 -0500

    Challenge solved.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 # Resolve the problem!!
-
+# -*- coding: utf-8 -*-
+import re
 
 def run():
-    # Start coding here
+    with open('encoded.txt', 'r') as f:
+        hidden_text = f.read()
+    
+    regex = re.compile('[a-z]')
+    show_message = re.findall(regex, hidden_text)
+
+    join_message = ''.join(show_message)
+    print(join_message)
 
 
 if __name__ == '__main__':
